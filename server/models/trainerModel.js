761dd9258157272398b9f3289c89caa28af81624
Meenaa <meenaasrv123@email.com>
2026-09-23 17:41:59 +0530

    update backend APIs

--- server/models/trainerModel.js
+++ server/models/trainerModel.js
@@ -1,10 +1,13 @@
 const trainers = [
   {
     id: 1,
-    name: "Rajesh Kumar",
+    name: "Rajesh Kumar.A",
     designation: "Senior Java Trainer",
     experience: "8 Years",
     specialization: "Java, Spring Boot, React",
+    image:
+      "https://images.unsplash.com/photo-1560250097-0b93528c311a?w=500"
+  
   },
   {
     id: 2,
@@ -12,6 +15,8 @@ const trainers = [
     designation: "Full Stack Trainer",
     experience: "6 Years",
     specialization: ".NET, C#, ASP.NET Core",
+    image:
+      "https://images.unsplash.com/photo-1580489944761-15a19d654956?w=500"
   },
   {
     id: 3,
@@ -19,6 +24,8 @@ const trainers = [
     designation: "Python Trainer",
     experience: "5 Years",
     specialization: "Python, Django, REST API",
+    image:
+      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=500"
   },
 ];
 
